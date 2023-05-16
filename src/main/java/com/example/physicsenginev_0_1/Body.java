c4c638ac9d2e226a1f350373fcd0a91ab9191adf
ec-ac <ecalaniz@uamv.edu.ni>
2023-05-16 16:37:04 -0600

    change to plans for Body vector calculations

--- src/main/java/com/example/physicsenginev_0_1/Body.java
+++ src/main/java/com/example/physicsenginev_0_1/Body.java
@@ -1,6 +1,15 @@
 package com.example.physicsenginev_0_1;
 
 public class Body {
+
+    /**
+     * i will just do this in the most "natural way" that i can think of
+     * this will mostly follow a Procedural mindset which i then will attempt to adapt more efficiently
+     */
+
+
+
+
     //a single body must have a size, shape, area
 
     //it must have a mass, and current vectors describing magnitude and direction
@@ -20,35 +29,109 @@ public class Body {
     //an object and its properties should be handled from any physics class, any task that would print the object on
     //screen should simply take the properties from its physics and then print it.
 
- /*   final double g = 9.81;
-    double prevPosition;
-    double position;
-    double mass;
+    final double g = -9.81;
+    double yPos;
+    double xPos;
+    final double mass = 10;
+    double xVelocity;
+    double yVelocity;
+    double yAccel;
+    double xAccel;
 
     Body body = new Body();
 
-    public double Velocity () {
-        double velocity = body.position - body.prevPosition;
-        return velocity;
+    double weight = g * mass;
+
+    //affector? these will return current net force to be applied to the object
+    public double vAffector(double exForce1) {
+        double netForce = g + exForce1;
+        return netForce;
     }
 
-    public double Acceleration(double prevVelocity, double velocity) {
-        double acceleration = velocity - prevVelocity;//wrong lol
-        return acceleration;
+    public double hAffector(double exForce1) {
+        double netForce = g + exForce1;
+        return netForce;
     }
 
-    public double NetForce (double acceleration, double mass) {
-        double force = acceleration * mass;
-        return force;
+    public double getG() {
+        return g;
     }
-*/
-    //this is wrong lol
 
-    //affector?
+    public double getyPos() {
+        return yPos;
+    }
 
-    /*public double Affector(double extrernalForce) {
-        return null;
-    }*/
+    //position thresholds must be implemented
+    public void setyPos(double yPos) {
+        this.yPos = body.getyPos() + body.getyVelocity()/60;
+    }
+
+    public double getxPos() {
+        return xPos;
+    }
+
+    //position thresholds must be implemented
+    public void setxPos(double xPos) {
+        this.xPos = body.getxPos() + body.getyVelocity()/60;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public double getxVelocity() {
+        return xVelocity;
+    }
+
+    public void setxVelocity(double xVelocity) {
+        this.xVelocity = body.getxVelocity() + body.getyAccel()/60;
+    }
+
+    public double getyVelocity() {
+        return yVelocity;
+    }
+
+    public void setyVelocity(double yVelocity) {
+        this.yVelocity = body.getyVelocity() + body.getyAccel()/60;
+    }
+
+    public double getyAccel() {
+        return yAccel;
+    }
+
+    public void setyAccel(double yAccel) {
+        this.yAccel = hAffector(body.getyForce()) / mass; //fix later
+    }
+
+    public double getxAccel() {
+        return xAccel;
+    }
+
+    public void setxAccel(double xAccel) {
+        this.xAccel = hAffector(body.getxForce()) / mass; //fix later
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public void setBody(Body body) {
+        this.body = body;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = mass * g;
+    }
+
+    //there is probably a better way of setting this such that the v and h forces will be calc'ed in a single function
+    //probably some data struct that im not using
+
+
+
 
     //scratch all of this:
     //any object will start with an initial velocity and acceleration of 0, at some position in the x,y plane.
@@ -68,6 +151,13 @@ public class Body {
     //a force affector will work to change acceleration
     //change in acceleration will always be net force / mass
         //net force will always be the sum of all forces.
-        //because this is primitive, we will only use 3 forces at a time for any y and 2 for any x net force
+        //because this is primitive, we will only use 2 forces at a time for any y and 1 for any x net force
+
+
+    //a
+
+
+    //there will be a single method executed once every 1/60 seconds that will update all these parameters
+
 
 }
